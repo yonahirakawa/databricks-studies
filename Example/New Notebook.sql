dbc3360843ac5ba4ac49cd1f610d99492f792219
yonahirakawa <yonahirakawa>
2022-10-25 13:56:22 +0000

    Modified the new notebook

---
+++ Example/New Notebook.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ## Delta Table Example
+
+-- COMMAND ----------
+
+-- MAGIC %sql
+-- MAGIC CREATE TABLE students
+-- MAGIC   (id INT, name STRING, value DOUBLE);
